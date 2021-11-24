634e040665056297daea15adac13e78320556a53
Lee Thompson <thompsonl@logh.net>
2021-11-23 21:06:19 -0800

    Add files via upload
    * Now supports arguments as arrays per spec (e.g. $yeelight->get_prop(["power", "bright", "rgb" ])->commit(); )
    * Port is now optional (defaults to 55443)
    * Timeout can now be set as a third parameter when using the constructor (default is 30 seconds)
    * Sleep between commands can now be set using the setDelay method minimum value is 50, maximum is 99999.   setDelay() resets to default of 100.
    * You can enable debug messages with the setDebug method.  Argument is true to enable, false to disable.

--- Yeelight.class.php
+++ Yeelight.class.php
@@ -2,17 +2,21 @@
 class Yeelight {
 	private $jobs = array();
 
-	public function __construct($ip, $port) {
+	public function __construct($ip, $port = 55443, $timeout = 30) {
 		$this->ip = $ip;
 		$this->port = $port;
-		if (!$this->verifyConnection()) throw new Exception("Failed connecting to Yeelight device.");
+		$this->delay = 100;
+		$this->debug = false;
+		$this->timeout = $timeout;
+		if(!$this->verifyConnection()) throw new Exception("Failed connecting to Yeelight device.");
 	}
 
 	private function verifyConnection() {
-		$this->fp = fsockopen($this->ip, $this->port, $this->errno, $this->errstr, 30);
-		if (!$this->fp) return false;
+		$this->fp = fsockopen($this->ip, $this->port, $this->errno, $this->errstr, $this->timeout);
+		if(!$this->fp) return false;
 
 		stream_set_blocking($this->fp, false);
+		if($this->debug) print "[YEELIGHT] [DEBUG] [CONNECTED] To $this->ip:$this->port ($this->timeout)\n";
 		return true;
 	}
 
@@ -20,13 +24,25 @@ class Yeelight {
 		if(!empty($this->jobs)) return count($this->jobs);
 		else return 0;
 	}
+	
+	public function setDelay($value = 100) {
+		$this->delay = $value;
+		if($this->delay < 50) $this->delay = 50;
+		if($this->delay > 99999) $this->delay = 99999;
+		return true;
+	}
+	
+	public function setDebug($value = false) {
+		if($value) $this->debug = true;
+		else $this->debug = false;
+		return true;
+	}
 
 	public function __call($method, $args) {
 		$jObj = new stdClass;
 		$jObj->id = $this->getNextID();
 		$jObj->method = $method;
 		$jObj->params = $args;
-
 		$this->jobs[] = $jObj;
 		return $this;
 	}
@@ -34,15 +50,15 @@ class Yeelight {
 	public function commit() {
 		if (!$this->verifyConnection()) throw new Exception("Failed connecting to Yeelight device.");
 		foreach($this->jobs as $job) {
+			if(count($job->params[0]) > 1) $job->params = $job->params[0];
 			$jStr = json_encode($job);
+			if($this->debug) print "[YEELIGHT] [DEBUG] [COMMIT] $jStr\n";
 			fwrite($this->fp, $jStr . "\r\n");
 			fflush($this->fp);
-
-			usleep(100 * 1000);
-
+			if($this->debug) print "[YEELIGHT] [DEBUG] [COMMIT] Sleeping for " . $this->delay * 1000 . " microseconds\n";
+			usleep($this->delay * 1000);
 			$out[] = fgets($this->fp);
 		}
-
 		$this->jobs = array();
 		if(!empty($out)) return $out;
 		else return true;
@@ -50,5 +66,6 @@ class Yeelight {
 
 	public function disconnect() {
 		fclose($this->fp);
+		if($this->debug) print "[YEELIGHT] [DEBUG] [DISCONNECTED] From $this->ip:$this->port\n";
 	}
 }
